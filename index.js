064ebd71bf79e6541e086d2987384204394b9223
ziyaudheenms <msharafudeen193@gmail.com>
2025-10-24 17:46:17 +0530

    feat : add CI Pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("hello devops");
+console.log('I am learning CI/CD with Github Actions.')
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log('Staring the test');
+setTimeout(() => {
+    console.log('wating for three seconds.');
+    
+}, 3000);
+console.log('Test completed');
